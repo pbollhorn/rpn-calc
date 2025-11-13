c36ffb724714499731271c422d7df7f3a37b520d
Peter Bollhorn <pbollhorn@gmail.com>
2025-11-13 15:19:16 +0100

    second commit

--- rpncalc.js
+++ rpncalc.js
@@ -1,1 +1,23 @@
 const expression = "2 3 4 x +";
+
+const inputQueue = [];
+
+const resultStack = [];
+
+//parseExpression læser en expression og putter den i inputQueue
+function parseExpression(expression) {
+    
+}
+
+// går gennem køen og finder tal og operationer
+function goThroughQueue() {}
+
+// udfører en bestemt operation
+function performOperation(operation) {}
+
+function rpncalc(expression) {
+  parseExpression(expression);
+  goThroughQueue();
+}
+
+rpncalc(expression);
